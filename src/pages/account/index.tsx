ebaf14e7f696277a76502c72372eb3b0da26a7c0
tj <thinupavani@gmail.com>
2024-04-08 07:56:05 +0530

    account form update

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -28,9 +28,18 @@ import Close from 'mdi-material-ui/Close'
 
 // ** Third Party Styles Imports
 import 'react-datepicker/dist/react-datepicker.css'
+import { OutlinedInput, InputAdornment } from '@mui/material'
+import { EyeOutline, EyeOffOutline } from 'mdi-material-ui'
 
 
-
+interface State {
+  newPassword: string
+  currentPassword: string
+  showNewPassword: boolean
+  confirmNewPassword: string
+  showCurrentPassword: boolean
+  showConfirmNewPassword: boolean
+}
 
 const ImgStyled = styled('img')(({ theme }) => ({
   width: 120,
@@ -59,7 +68,7 @@ const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
 const AccountSettings = () => {
 
 
-  const [openAlert, setOpenAlert] = useState<boolean>(true)
+  // const [openAlert, setOpenAlert] = useState<boolean>(true)
   const [imgSrc, setImgSrc] = useState<string>('/images/avatars/1.png')
 
   const onChange = (file: ChangeEvent) => {
@@ -72,12 +81,60 @@ const AccountSettings = () => {
     }
   }
 
+
+
+
+  // ** States
+  const [values, setValues] = useState<State>({
+    newPassword: '',
+    currentPassword: '',
+    showNewPassword: false,
+    confirmNewPassword: '',
+    showCurrentPassword: false,
+    showConfirmNewPassword: false
+  })
+
+  // Handle Current Password
+  const handleCurrentPasswordChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+  const handleClickShowCurrentPassword = () => {
+    setValues({ ...values, showCurrentPassword: !values.showCurrentPassword })
+  }
+  const handleMouseDownCurrentPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
+  // Handle New Password
+  const handleNewPasswordChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+  const handleClickShowNewPassword = () => {
+    setValues({ ...values, showNewPassword: !values.showNewPassword })
+  }
+  const handleMouseDownNewPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
+  // Handle Confirm New Password
+  const handleConfirmNewPasswordChange = (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+  const handleClickShowConfirmNewPassword = () => {
+    setValues({ ...values, showConfirmNewPassword: !values.showConfirmNewPassword })
+  }
+  const handleMouseDownConfirmNewPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
   return (
     <Card>
       <CardContent>
         <form>
+
           <Grid container spacing={7}>
             <Grid item xs={12} sm={6}>
+              <Typography style={{ fontWeight: 600, marginBottom: '20px' }}>User Details</Typography>
               <TextField fullWidth label='Username' placeholder='johnDoe' defaultValue='johnDoe' style={{ marginBottom: '20px' }} />
               <Grid container spacing={4}>
                 <Grid item xs={12} sm={6}>
@@ -120,8 +177,133 @@ const AccountSettings = () => {
                   </FormControl>
                 </Grid>
               </Grid>
+              <Grid container spacing={4} style={{ marginTop: '20px' }}>
+                <Grid item xs={12} sm={6}>
+                  <TextField fullWidth label='Company' placeholder='ABC Pvt. Ltd.' defaultValue='ABC Pvt. Ltd.' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField fullWidth label='Vat Number' placeholder='Vat Number' defaultValue='123' />
+                </Grid>
+              </Grid>
+              <Grid container spacing={4} style={{ marginTop: '20px' }}>
+                <Grid item xs={12} sm={6}>
+                  <TextField fullWidth label='Billing Address' placeholder='Billing Address' defaultValue='ABC' />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField fullWidth label='Location' placeholder='Location' defaultValue='ABC' />
+                </Grid>
+              </Grid>
+              <Grid item xs={12} style={{ marginTop: '40px' }}>
+                <Button variant='contained' sx={{ marginRight: 3.5 }}>
+                  Save Changes
+                </Button>
+                <Button type='reset' variant='outlined' color='secondary'>
+                  Reset
+                </Button>
+              </Grid>
+
+              <Typography style={{ fontWeight: 600, marginBottom: '20px', marginTop: '40px' }}>Change Password</Typography>
+              <CardContent sx={{ paddingBottom: 0 }}>
+                <Grid container spacing={5}>
+                  <Grid item xs={12} sm={12}>
+                    <Grid container spacing={5}>
+                      <Grid item xs={12} sx={{ marginTop: 4.75 }}>
+                        <FormControl fullWidth>
+                          <InputLabel htmlFor='account-settings-current-password'>Current Password</InputLabel>
+                          <OutlinedInput
+                            label='Current Password'
+                            value={values.currentPassword}
+                            id='account-settings-current-password'
+                            type={values.showCurrentPassword ? 'text' : 'password'}
+                            onChange={handleCurrentPasswordChange('currentPassword')}
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <IconButton
+                                  edge='end'
+                                  aria-label='toggle password visibility'
+                                  onClick={handleClickShowCurrentPassword}
+                                  onMouseDown={handleMouseDownCurrentPassword}
+                                >
+                                  {values.showCurrentPassword ? <EyeOutline /> : <EyeOffOutline />}
+                                </IconButton>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+                      </Grid>
+
+                      <Grid item xs={12} sx={{ marginTop: 6 }}>
+                        <FormControl fullWidth>
+                          <InputLabel htmlFor='account-settings-new-password'>New Password</InputLabel>
+                          <OutlinedInput
+                            label='New Password'
+                            value={values.newPassword}
+                            id='account-settings-new-password'
+                            onChange={handleNewPasswordChange('newPassword')}
+                            type={values.showNewPassword ? 'text' : 'password'}
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <IconButton
+                                  edge='end'
+                                  onClick={handleClickShowNewPassword}
+                                  aria-label='toggle password visibility'
+                                  onMouseDown={handleMouseDownNewPassword}
+                                >
+                                  {values.showNewPassword ? <EyeOutline /> : <EyeOffOutline />}
+                                </IconButton>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+                      </Grid>
+
+                      <Grid item xs={12}>
+                        <FormControl fullWidth>
+                          <InputLabel htmlFor='account-settings-confirm-new-password'>Confirm New Password</InputLabel>
+                          <OutlinedInput
+                            label='Confirm New Password'
+                            value={values.confirmNewPassword}
+                            id='account-settings-confirm-new-password'
+                            type={values.showConfirmNewPassword ? 'text' : 'password'}
+                            onChange={handleConfirmNewPasswordChange('confirmNewPassword')}
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <IconButton
+                                  edge='end'
+                                  aria-label='toggle password visibility'
+                                  onClick={handleClickShowConfirmNewPassword}
+                                  onMouseDown={handleMouseDownConfirmNewPassword}
+                                >
+                                  {values.showConfirmNewPassword ? <EyeOutline /> : <EyeOffOutline />}
+                                </IconButton>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+
+                </Grid>
+              </CardContent>
+              <Box sx={{ mt: 11 }}>
+                <Button variant='contained' sx={{ marginRight: 3.5 }}>
+                  Save Changes
+                </Button>
+                <Button
+                  type='reset'
+                  variant='outlined'
+                  color='secondary'
+                  onClick={() => setValues({ ...values, currentPassword: '', newPassword: '', confirmNewPassword: '' })}
+                >
+                  Reset
+                </Button>
+              </Box>
+
+              
             </Grid>
             <Grid item xs={12} sm={6} sx={{ marginTop: 4.8, marginBottom: 3 }}>
+              <Typography style={{ fontWeight: 600, marginBottom: '20px' }}>Profile Photo</Typography>
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <ImgStyled src={imgSrc} alt='Profile Pic' />
                 <Box>
@@ -145,48 +327,7 @@ const AccountSettings = () => {
               </Box>
             </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label='Username' placeholder='johnDoe' defaultValue='johnDoe' />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label='Name' placeholder='John Doe' defaultValue='John Doe' />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                type='email'
-                label='Email'
-                placeholder='johnDoe@example.com'
-                defaultValue='johnDoe@example.com'
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Role</InputLabel>
-                <Select label='Role' defaultValue='admin'>
-                  <MenuItem value='admin'>Admin</MenuItem>
-                  <MenuItem value='author'>Author</MenuItem>
-                  <MenuItem value='editor'>Editor</MenuItem>
-                  <MenuItem value='maintainer'>Maintainer</MenuItem>
-                  <MenuItem value='subscriber'>Subscriber</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Status</InputLabel>
-                <Select label='Status' defaultValue='active'>
-                  <MenuItem value='active'>Active</MenuItem>
-                  <MenuItem value='inactive'>Inactive</MenuItem>
-                  <MenuItem value='pending'>Pending</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label='Company' placeholder='ABC Pvt. Ltd.' defaultValue='ABC Pvt. Ltd.' />
-            </Grid>
-
-            {openAlert ? (
+            {/* {openAlert ? (
               <Grid item xs={12} sx={{ mb: 3 }}>
                 <Alert
                   severity='warning'
@@ -203,16 +344,9 @@ const AccountSettings = () => {
                   </Link>
                 </Alert>
               </Grid>
-            ) : null}
+            ) : null} */}
 
-            <Grid item xs={12}>
-              <Button variant='contained' sx={{ marginRight: 3.5 }}>
-                Save Changes
-              </Button>
-              <Button type='reset' variant='outlined' color='secondary'>
-                Reset
-              </Button>
-            </Grid>
+
           </Grid>
         </form>
       </CardContent>
