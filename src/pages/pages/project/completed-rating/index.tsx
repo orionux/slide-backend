bc5c43d2dfda09a05d4dfad7e12c4916a2902bd3
tj <thinupavani@gmail.com>
2024-04-20 12:34:47 +0530

    update rating

--- src/pages/pages/project/completed-rating/index.tsx
+++ src/pages/pages/project/completed-rating/index.tsx
@@ -1,16 +1,97 @@
-import { Typography } from '@mui/material'
-import React, { ReactNode } from 'react'
+import { Box, Grid, Typography } from '@mui/material'
+import React, { ReactNode, useState } from 'react'
 import BlankLayout from 'src/@core/layouts/BlankLayout'
 import NormalHeader from 'src/layouts/components/NormalHeader'
 import ProjectFooter from 'src/views/projectComponents/projectFooter'
 import ProjectHeader from 'src/views/projectComponents/projectHeader'
+import { HiOutlineStar, HiMiniStar  } from "react-icons/hi2";
+
+
+
+
+interface Rating {
+    [key: string]: string | null;
+}
 
 const Completed = () => {
+
+    const [selectedRating, setSelectedRating] = useState<Rating>({});
+
+    const handleRatingClick = (question: string, rating: string) => {
+        setSelectedRating(prevState => ({
+            ...prevState,
+            [question]: rating
+        }));
+        console.log("Selected rating for", question + ":", rating);
+    };
+
+    const ratingData = [
+        {
+            id: 1,
+            question: 'Did the quality of design meet your expectations?',
+            iconArray: ['VERY BAD', 'POOR', 'AS EXPECTED', 'GREAT', 'EXCELLENT']
+        },
+        {
+            id: 2,
+            question: 'How well did we Understand the requirements ?',
+            iconArray: ['VERY BAD', 'POOR', 'AS EXPECTED', 'GREAT', 'EXCELLENT']
+        },
+        {
+            id: 3,
+            question: 'How well did we adhere to the deadline ?',
+            iconArray: ['VERY BAD', 'POOR', 'AS EXPECTED', 'GREAT', 'EXCELLENT']
+        },
+        {
+            id: 4,
+            question: 'How well did we pay attention to the details ?',
+            iconArray: ['VERY BAD', 'POOR', 'AS EXPECTED', 'GREAT', 'EXCELLENT']
+        }
+    ];
+
+
     return (
         <>
             <NormalHeader />
             <ProjectHeader />
-            <Typography>Completed</Typography>
+            <Box sx={{ backgroundColor: '#F2F5FF', padding: '50px 100px' }}>
+                <Typography sx={{ textTransform: 'uppercase', fontSize: '20px', fontWeight: 500, fontFamily: "'Syne', sans-serif !important;" }}>Completed</Typography>
+                <Typography sx={{ fontSize: '14px', fontWeight: 500, fontFamily: 'Inter, sans-serif;' }}>
+                    Your Presentation is done visit to the project page . In the meantime, please Leave us some feedback, It helps us improve our service  to you!
+                </Typography>
+
+                <Box sx={{ padding: '50px 0px', display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '16px', fontWeight: 500, fontFamily: 'Inter, sans-serif;', marginBottom: '30px' }}>
+                        Your Honest feedback will help our designers improve
+                    </Typography>
+                    
+                    {
+                        ratingData.map((item) => (
+                            <Box key={item.id} sx={{ display: 'flex', flexDirection: 'column', marginBottom: '30px' }}>
+                                <Typography sx={{ fontSize: '14px', fontWeight: 500, fontFamily: 'Inter, sans-serif;' }}>
+                                    {item.question}
+                                </Typography>
+                                <Grid container spacing={10} sx={{ margin: '0px !important' }}>
+                                    {
+                                        item.iconArray.map((rate, index) => (
+                                            <Grid item xs={2} key={index} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                                <button onClick={() => handleRatingClick(item.question, rate)}>
+                                                    {
+                                                        selectedRating[item.question] === rate ? <HiMiniStar style={{fontSize: '30px', color: '#FFC107'}} />: <HiOutlineStar  style={{fontSize: '30px', color: '#455A64'}} />
+                                                    }
+                                                    
+                                                    <Typography sx={{ fontFamily: 'Inter, sans-serif;', fontSize: '10px', fontWeight: 600, color: selectedRating[item.question] === rate ? '#FFC107' : '#455A64' }}>
+                                                        {rate}
+                                                    </Typography>
+                                                </button>
+                                            </Grid>
+                                        ))
+                                    }
+                                </Grid>
+                            </Box>
+                        ))
+                    }
+                </Box>
+            </Box>
             <ProjectFooter />
         </>
     )
